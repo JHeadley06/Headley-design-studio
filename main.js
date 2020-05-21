c55485e5a7612a36021ba944117b62f3c7f955ed
JHeadley06 <JHeadley06@users.noreply.github.com>
2020-05-21 10:07:03 +0100

    menu animation

--- main.js
+++ main.js
@@ -1,7 +1,37 @@
-document.getElementById("menu-trigger").addEventListener("click", menuDisplay);
+let toggleNavStatus = false;
 
-function menuDisplay() {
-    document.getElementById("sub-menu").classList.toggle("active");
-    document.getElementById("menu-icon").classList.toggle("active");
-    document.getElementById("x-icon").classList.toggle("active");
+function openMenu() {
+    let getSidebar = document.querySelector("#sub-menu");
+    let getSideLinks = document.querySelectorAll("a.menu-li");
+    let getMenuIcon = document.querySelector("#menu-icon");
+    let getXIcon = document.querySelector("#x-icon");
+
+    if (toggleNavStatus === false) {
+        getSidebar.style.width = "300px";
+        getMenuIcon.classList.add("active");
+        getXIcon.classList.add("active");
+
+        let liLength = getSideLinks.length;
+        for (let i = 0; i < liLength; i++) {
+            getSideLinks[i].style.visibility = "visible";
+            getSideLinks[i].style.opacity = "1";
+        }
+    
+        toggleNavStatus = true;
+
+    }   else if (toggleNavStatus === true) {
+
+        let liLength = getSideLinks.length;
+        for (let i = 0; i < liLength; i++) {
+            getSideLinks[i].style.visibility = "hidden";
+            getSideLinks[i].style.opacity = "0";
+        }
+
+        getXIcon.classList.remove("active");
+        getMenuIcon.classList.remove("active");
+        getSidebar.style.width = "100px";
+        
+        toggleNavStatus = false;
+    }
+
 }
